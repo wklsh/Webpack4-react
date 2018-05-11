2160532d9a8619635a4a6fa3c04629a4777ce92f
Shawn lim wee kiang <wklsh@hotmail.sg>
2018-05-11 11:23:01 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//// Base Config for webpack. 
+//// Base Config for webpack.
 //// - Settings in here executes on both Dev and Prod env.
 //// -----------------------------------------------------------------
 //// ** DO NOT INCLUDE DEV / PROD SPECIFIC SETTINGS IN HERE ***
@@ -9,6 +9,10 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  // output: {
+  //   publicPath: '/'
+  // },
+
   devServer: {
     historyApiFallback: true,
     inline: true,
@@ -23,11 +27,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.[s]css?$/, 
+        test: /\.[s]css?$/,
         exclude: path.resolve(__dirname, 'node_modules'),
         use: ExtractTextPlugin.extract({
           use: [
-            { loader: "css-loader", options: { url: false, minimize: false, sourceMap: (process.env.NODE_ENV == 'development' ? true : false) }},
+            { loader: "css-loader", options: { url: true, minimize: false, sourceMap: (process.env.NODE_ENV == 'development' ? true : false) }},
             { loader: "postcss-loader", options: { sourceMap: (process.env.NODE_ENV == 'development' ? true : false) }},
             { loader: "sass-loader", options: { sourceMap: (process.env.NODE_ENV == 'development' ? true : false) }}
           ],
@@ -36,16 +40,34 @@ module.exports = {
       },
 
       {
-        test: /\.js[x]?$/, 
+        test: /\.js[x]?$/,
         include: path.resolve(__dirname, 'src'),
         exclude: path.resolve(__dirname, 'node_modules'),
         loader: 'babel-loader'
       },
+
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          { loader: 'file-loader',
+            options: {
+              publicPath: path.resolve(__dirname, '/'),
+              outputPath: path.resolve(__dirname, '/'),
+              context: path.resolve(__dirname, 'src'),
+              name: '[path][name].[ext]',
+            }
+          },
+        ]
+      }
     ]
   },
-  
+
   resolve: {
-    extensions: ['.css', '.scss', '.js', '.jsx']
+    extensions: ['.css', '.scss', '.js', '.jsx'],
+    alias: {
+      BaseAlias: path.resolve(__dirname, 'src/app/base'),
+      ImgAlias: path.resolve(__dirname, 'src/img'),
+    }
   },
 
   plugins: [
