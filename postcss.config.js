bd5f9e30b6cd1c51040627fd34c90e7146b8fc92
shawn lim <shawn@codigo.sg>
2018-08-16 15:21:36 +0800

    npm updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 module.exports = {
 	plugins: {
+		"postcss-flexbugs-fixes": {},
+
+		"css-mqpacker": {},
+
 		autoprefixer: {
 			browsers: ["defaults"]
 		},
 		cssnano: {
 			zindex: false,
 			reduceIdents: false
-		},
-
-		"postcss-flexbugs-fixes": {},
-
-		"css-mqpacker": {}
+		}
 	}
 };
