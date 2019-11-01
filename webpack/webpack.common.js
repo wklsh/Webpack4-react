4dd01140f7ce8920ab99d078a08c1d172cca742b
WKLSH <wklsh@hotmail.sg>
2019-11-02 03:49:44 +0800

    feat: Rework deployment method and update affected boilerplate files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,156 +1,187 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/app/index.jsx'),
+	entry: path.resolve(__dirname, "../src/app/index.jsx"),
 
-  output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, '../dist'),
-  },
+	output: {
+		filename: "js/[name].js",
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.txt$/,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name:
-                process.env.NODE_ENV !== 'production'
-                  ? '[path][name].[ext]'
-                  : '[sha512:hash:base64:7].[ext]',
-              outputPath: process.env.NODE_ENV !== 'production' ? 'images/' : 'assets/',
-              publicPath: process.env.NODE_ENV !== 'production' ? 'images/' : 'assets/',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name:
-                process.env.NODE_ENV !== 'production'
-                  ? '[path][name].[ext]'
-                  : '[sha512:hash:base64:7].[ext]',
-              outputPath: process.env.NODE_ENV !== 'production' ? 'fonts/' : 'assets/',
-              publicPath: process.env.NODE_ENV !== 'production' ? 'fonts/' : 'assets/',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName:
-                process.env.NODE_ENV !== 'production' ? '[name]---[local]' : '[hash:base64:8]',
-              camelCase: true,
-              url: false,
-              sourceMap: process.env.NODE_ENV !== 'production',
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: process.env.NODE_ENV !== 'production',
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: process.env.NODE_ENV !== 'production',
-            },
-          },
-        ],
-      },
+	module: {
+		rules: [
+			{
+				test: /\.txt$/,
+				use: "raw-loader",
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader",
+						options: {
+							minimize: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "[path][name].[ext]"
+									: "[sha512:hash:base64:7].[ext]",
+							outputPath:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "images/"
+									: "assets/",
+							publicPath:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "images/"
+									: "assets/",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(woff|woff2|ttf|otf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "[path][name].[ext]"
+									: "[sha512:hash:base64:7].[ext]",
+							outputPath:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "fonts/"
+									: "assets/",
+							publicPath:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "fonts/"
+									: "assets/",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: process.env.NODE_ENV === "development",
+						},
+					},
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							localIdentName:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+									? "[name]---[local]"
+									: "[hash:base64:8]",
+							camelCase: true,
+							url: false,
+							sourceMap:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging",
+						},
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							sourceMap:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging",
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap:
+								process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging",
+						},
+					},
+				],
+			},
 
-      {
-        test: /\.js[x]?$/i,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              emitWarning: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
+			{
+				test: /\.js[x]?$/i,
+				exclude: path.resolve(__dirname, "node_modules"),
+				use: [
+					{
+						loader: "babel-loader",
+					},
+					{
+						loader: "eslint-loader",
+						options: {
+							emitWarning: true,
+						},
+					},
+				],
+			},
+		],
+	},
 
-  resolve: {
-    alias: {
-      Src: path.resolve(__dirname, '../src'),
-      Redux: path.resolve(__dirname, '../src/app/redux'),
-      Styles: path.resolve(__dirname, '../src/app/base/styles'),
-    },
-    extensions: ['.jsx', '.js', '.scss', '.css'],
-  },
+	resolve: {
+		alias: {
+			Src: path.resolve(__dirname, "../src"),
+			Redux: path.resolve(__dirname, "../src/app/redux"),
+			Styles: path.resolve(__dirname, "../src/app/base/styles"),
+		},
+		extensions: [".jsx", ".js", ".scss", ".css"],
+	},
 
-  plugins: [
-    new CleanWebpackPlugin(),
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+		}),
 
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
+		new PreloadWebpackPlugin({
+			rel: "preload",
+			as(entry) {
+				if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return "font";
+			},
+			fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
+			include: "allAssets",
+		}),
 
-    new PreloadWebpackPlugin({
-      rel: 'preload',
-      as(entry) {
-        if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return 'font';
-      },
-      fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
-      include: 'allAssets',
-    }),
+		new ScriptExtHtmlWebpackPlugin({
+			defaultAttribute: "defer",
+		}),
 
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer',
-    }),
+		new MiniCssExtractPlugin({
+			filename: "css/style.css",
+			chunkFilename:
+				process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging"
+					? "css/[id].css"
+					: "css/[id].[chunkhash:8].css",
+			disable: false,
+			allChunks: true,
+		}),
 
-    new MiniCssExtractPlugin({
-      filename: 'css/style.css',
-      chunkFilename:
-        process.env.NODE_ENV !== 'production' ? 'css/[id].css' : 'css/[id].[chunkhash:8].css',
-      disable: false,
-      allChunks: true,
-    }),
-  ],
+		new webpack.ProvidePlugin({
+			// 	$: "jquery",
+			// 	jquery: "jQuery",
+			// 	"window.$": "jquery",
+		}),
+
+		new CopyWebpackPlugin([
+			// { from: "./src/index.php", to: "index.php" },
+			// { from: "./src/meta.json", to: "meta.json" },
+			// { from: "./src/seo.php", to: "seo.php" },
+			// { from: "./src/.htaccess", to: ".htaccess", toType: "file" },
+			// { from: "./src/robots.txt", to: "robots.txt" },
+		]),
+	],
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,63 +1,67 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-const TerserPlugin = require('terser-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const imageminMozjpeg = require('imagemin-mozjpeg');
-
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const OfflinePlugin = require('offline-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const OfflinePlugin = require("offline-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+	mode: "production",
 
-  devtool: 'none',
+	devtool: "none",
 
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'js/[chunkhash:8].js',
-  },
+	output: {
+		path: path.resolve(__dirname, "../build/dist"),
+		filename: "js/[chunkhash:8].js",
+	},
 
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        test: /\.js(\?.*)?$/i,
-        parallel: true,
-      }),
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				test: /\.js(\?.*)?$/i,
+				parallel: true,
+			}),
 
-      new OptimizeCSSAssetsPlugin(),
-    ],
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+			new OptimizeCSSAssetsPlugin(),
+		],
+		splitChunks: {
+			chunks: "all",
+		},
+	},
 
-  plugins: [
-    new ImageminPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      gifsicle: {
-        // lossless gif compressor
-        optimizationLevel: 9,
-      },
-      pngquant: {
-        // lossy png compressor, remove for default lossless
-        quality: '75',
-      },
-      plugins: [
-        imageminMozjpeg({
-          // lossy jpg compressor, remove for default lossless
-          quality: '75',
-        }),
-      ],
-    }),
+	plugins: [
+		new CleanWebpackPlugin({
+			path: path.resolve(__dirname, "../build/dist"),
+		}),
 
-    new OfflinePlugin(),
-    
-    new FaviconsWebpackPlugin({
+		new ImageminPlugin({
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			gifsicle: {
+				// lossless gif compressor
+				optimizationLevel: 9,
+			},
+			pngquant: {
+				// lossy png compressor, remove for default lossless
+				quality: "75",
+			},
+			plugins: [
+				imageminMozjpeg({
+					// lossy jpg compressor, remove for default lossless
+					quality: "75",
+				}),
+			],
+		}),
+
+		new OfflinePlugin(),
+
+		new FaviconsWebpackPlugin({
 			logo: "./src/favicon.png",
 			prefix: "favicons/",
 		}),
-  ],
+	],
 });
